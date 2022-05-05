3f970bd85d8f8c330c768b5a4b952498b8042d13
An Qi Zhang <guy_asdf@hotmail.com>
2022-05-06 00:55:42 +0100

    some more testing lean..

--- lean-examples/inductive-of-inductive-example.lean
+++ lean-examples/inductive-of-inductive-example.lean
@@ -6,13 +6,14 @@ inductive structure_description
 inductive AST
 -- | struct_descript : structure_description → AST
 | struct_descript : structure_description → AST
--- | none -- is this right? can I say none here?
+| none -- is this right? can I say none here?
 
 -- ================ inner example (structure_description)
 
-def get_inner (sth : structure_description) : Nat :=
+def get_inner (sth : Option structure_description) : Option Nat :=
 match sth with
 | structure_description.thing x => x
+| none => none
 
 def ex1 : structure_description := structure_description.thing 4
 
@@ -21,9 +22,10 @@ def ex1 : structure_description := structure_description.thing 4
 
 -- ================== outter example (AST example)
 
-def get_inner_ast (sth : AST) : structure_description :=
+def get_inner_ast (sth : AST) : Option structure_description :=
 match sth with
 | AST.struct_descript stuff => stuff
+| AST.none => none
 
 def ex2 : AST := AST.struct_descript ( structure_description.thing 3 )
 #check get_inner_ast ex2
@@ -32,3 +34,22 @@ def ex2 : AST := AST.struct_descript ( structure_description.thing 3 )
 #eval get_inner ( get_inner_ast ex2 )
 -- .structure_description.thing 4
 
+
+
+inductive list_test
+| somthing : List Nat → list_test
+
+def ex4 : list_test := list_test.somthing []
+
+def get_list (x : list_test) : List Nat :=
+match x with
+| list_test.somthing y => dbg_trace y
+y
+
+def get_list_item (x: List Nat) : Nat :=
+match x with
+| h :: t => h
+| [] => 
+
+#check ex4
+#eval get_list ex4
