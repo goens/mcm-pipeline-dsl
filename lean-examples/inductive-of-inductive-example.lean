2d01c7d95491c42e5b89a720644bf2ee3df0843f
An Qi Zhang <aq2zhang@uwaterloo.ca>
2022-05-06 20:43:22 +0100

    add some more sandbox examples

--- lean-examples/inductive-of-inductive-example.lean
+++ lean-examples/inductive-of-inductive-example.lean
@@ -54,3 +54,70 @@ match x with
 #check ex4
 #eval get_list ex4
 #eval get_list_item (get_list ex4)
+
+inductive factor
+| simple : Nat → factor
+| unary : String → factor → factor
+
+inductive expr
+| expr_factor : factor → expr
+| term : factor → List (String × factor) → expr
+
+def ex5 : expr := expr.expr_factor ( factor.simple 7 )
+def ex6 : factor := factor.simple 9
+def ex7 : factor := factor.unary "EY YO!!!"  ex6
+
+def get_inner_fact (sth : factor) : Nat :=
+match sth with
+| factor.simple y => y
+-- | factor.unary ls => get_inner_fact ls.2
+| factor.unary st sth => get_inner_fact sth --get_inner_fact ls.2
+
+def get_inner_expr (x : expr) : Nat :=
+match x with
+| expr.expr_factor n => get_inner_fact n
+| expr.term m (l :: ls) => get_inner_fact m
+| expr.term m [] => get_inner_fact m
+
+#check ex5
+#eval get_inner_expr ex5
+
+
+-- ========================================================
+
+inductive one_or_more_term
+| right_recursion : term → term_tail → one_or_more_term
+
+inductive term_tail
+| recursion : term → term_tail
+| zero : ε → term_tail
+
+inductive term
+| term : Nat → term
+
+inductive ε
+| nothing : String → ε
+
+-- ======= ONE TERM ======
+def ex8 : term := term.term 42
+def ex9 : one_or_more_term := one_or_more_term.right_recursion ex8 ""
+def ex10 : one_or_more_term := one_or_more_term.right_recursion 69 "heh"
+def ex11 : term_tail := term_tail.recursion ( one_or_more_term.right_recursion 42 "lol !" )
+-- ======= TWO TERMS? ======
+def ex12 : one_or_more_term := one_or_more_term.right_recursion 42 term_tail
+
+#check ex12
+
+-- =======================================================
+
+inductive item
+| something : item → item → item
+| int : Nat → item
+| nothing : item
+
+-- ==== ONE TERM ====
+def ex13 : item := item.int 1
+def ex14 : item := item.something ex13 item.nothing
+-- ==== TWO TERMS ====
+def ex15 : item := item.int 2
+def ex16 : item := item.something ex15 ex14 
