7e79d556a60fe0a62f4b38b589ba11c73354641e
Andres Goens <andres.goens@ed.ac.uk>
2022-05-09 12:23:08 +0100

    filestructure garndening

---
+++ PipelineDSL.lean
@@ -0,0 +1,2 @@
+import PipelineDSL.AST
+import PipelineDSL.Parser
--- lakefile.lean
+++ lakefile.lean
@@ -3,4 +3,13 @@ open Lake DSL
 
 package «pipeline-dsl» {
   -- add configuration options here
+  dependencies := #[{
+  name := `Graph
+  src := Source.git "https://github.com/goens/graph-library-for-lean4.git" "master"
+  },
+  {
+  name := `MLIR
+  src := Source.git "https://github.com/opencompl/lean-mlir" "master"
+  }
+  ]
 }
