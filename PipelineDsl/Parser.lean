170fe27304526eb5c2c0185b8ed34b5c3b1de1fd
Andres Goens <andres.goens@ed.ac.uk>
2022-05-17 14:02:10 +0100

    Implemented Quote and QuoteM for Const and Term
    Modified-by: Andres Goens <andres.goens@ed.ac.uk>

--- PipelineDsl/Parser.lean
+++ PipelineDsl/Parser.lean
@@ -1,6 +1,10 @@
 import Lean
 import PipelineDsl.AST
 open Lean Lean.Syntax
+open Lean.Elab
+open Lean.Elab.Command
+import Lean.Elab.Deriving.Basic
+open Lean.Meta
 
 namespace Pipeline
 
@@ -118,11 +122,11 @@ syntax "["  expr_list "]" : list
 -- parsing
 
 
-/- private -/ def mkParseFun {α : Type} (syntaxcat : Name) (ntparser : Syntax → Except String α) :
+private def mkParseFun {α : Type} (syntaxcat : Name) (ntparser : Syntax → Except String α) :
 String → Environment → Except String α := λ s env => do
   ntparser (← Parser.runParserCategory env syntaxcat s)
 
-/- private -/ def mkNonTerminalParser {α : Type} [Inhabited α] (syntaxcat : Name) (ntparser : Syntax → Except String α)
+private def mkNonTerminalParser {α : Type} [Inhabited α] (syntaxcat : Name) (ntparser : Syntax → Except String α)
 (s : String) (env : Environment) : Option String × α :=
   let parseFun := mkParseFun syntaxcat ntparser
   match parseFun s env with
@@ -394,8 +398,191 @@ syntax "[arg_list|" arg_list "]" : term
 syntax "[constval|" constval "]" : term
 syntax "[dsl_transition|" dsl_transition "]" : term
 
--- macro_rules
---   | [constval| $x:constval ] => mkConstval x
+class QuoteM (a: Type)  where
+  quoteM : a -> MacroM Syntax
+
+open QuoteM
+
+instance [Quote a] : QuoteM a where
+  quoteM a := return (quote a)
+
+
+-- abbrev MacroM := ReaderT Macro.Context (EStateM Macro.Exception Macro.State)
+
+def runMacroM (defaultA: a) (ma: MacroM a): a :=
+  let estate := ReaderT.run ma (Macro.Context.mk default `syntheticMainModule 0 0 99999 Syntax.missing)
+  let aopt := estate.run' (Macro.State.mk 0 [])
+  aopt.getD defaultA
+
+def quoteMConst : Const → MacroM Syntax
+ | .num_lit n => `(Const.num_lit $(quote n))
+ | .str_lit s => `(Const.str_lit $(quote s))
+
+
+
+instance : QuoteM Const where
+  quoteM := quoteMConst
+
+def quoteMTerm : Term -> MacroM Syntax
+| .negation t => do
+     `(Term.negation $(<- (quoteMTerm t)))
+| .logical_negation t => do
+     `(Term.logical_negation $(<- (quoteMTerm t)))
+| .const c => do
+  `(Term.const $(<- (quoteM c)))
+| _ => Macro.throwError "unhandled term case"
+
+instance : QuoteM Term where
+  quoteM := quoteMTerm
+
+instance [Inhabited a] [QuoteM a] : Quote a where
+  quote (val: a) := runMacroM default (quoteM val)
+
+
+macro_rules
+  | `([constval| $x:constval ]) => do
+     let val := mkConstval x
+     match val with
+       | .error msg => Macro.throwError msg
+       | .ok v => return (quote v)
+
+
+macro_rules
+| `([dsl_term| $x ]) => do
+   let val := mkTerm x
+   match val with
+   | .error msg => Macro.throwError msg
+   | .ok v => return (quote v)
+
+
+def foo := [constval| 42]     
+#reduce foo
+
+def bar := [dsl_term| 42]
+#reduce bar
+
+
+-- CUSTOM DERIVING
+/-
+private abbrev IndexSet := Std.RBTree Nat compare
+private abbrev LocalInst2Index := FVarIdMap Nat
+
+private def implicitBinderF := Parser.Term.implicitBinder
+private def instBinderF     := Parser.Term.instBinder
+
+private def mkInhabitedInstanceUsing (inductiveTypeName : Name) (ctorName : Name) (addHypotheses : Bool) : CommandElabM Bool := do
+  match (← liftTermElabM none mkInstanceCmd?) with
+  | some cmd =>
+    elabCommand cmd
+    return true
+  | none =>
+    return false
+where
+  addLocalInstancesForParamsAux {α} (k : LocalInst2Index → TermElabM α) : List Expr → Nat → LocalInst2Index → TermElabM α
+    | [], i, map    => k map
+    | x::xs, i, map =>
+      try
+        let instType ← mkAppM `Inhabited #[x]
+        if (← isTypeCorrect instType) then
+          withLocalDeclD (← mkFreshUserName `inst) instType fun inst => do
+            trace[Elab.Deriving.inhabited] "adding local instance {instType}"
+            addLocalInstancesForParamsAux k xs (i+1) (map.insert inst.fvarId! i)
+        else
+          addLocalInstancesForParamsAux k xs (i+1) map
+      catch _ =>
+        addLocalInstancesForParamsAux k xs (i+1) map
+
+  addLocalInstancesForParams {α} (xs : Array Expr) (k : LocalInst2Index → TermElabM α) : TermElabM α := do
+    if addHypotheses then
+      addLocalInstancesForParamsAux k xs.toList 0 {}
+    else
+      k {}
+
+  collectUsedLocalsInsts (usedInstIdxs : IndexSet) (localInst2Index : LocalInst2Index) (e : Expr) : IndexSet :=
+    if localInst2Index.isEmpty then
+      usedInstIdxs
+    else
+      let visit {ω} : StateRefT IndexSet (ST ω) Unit :=
+        e.forEach fun
+          | Expr.fvar fvarId _ =>
+            match localInst2Index.find? fvarId with
+            | some idx => modify (·.insert idx)
+            | none => pure ()
+          | _ => pure ()
+      runST (fun _ => visit |>.run usedInstIdxs) |>.2
+
+  /- Create an `instance` command using the constructor `ctorName` with a hypothesis `Inhabited α` when `α` is one of the inductive type parameters
+     at position `i` and `i ∈ assumingParamIdxs`. -/
+  mkInstanceCmdWith (assumingParamIdxs : IndexSet) : TermElabM Syntax := do
+    let indVal ← getConstInfoInduct inductiveTypeName
+    let ctorVal ← getConstInfoCtor ctorName
+    let mut indArgs := #[]
+    let mut binders := #[]
+    for i in [:indVal.numParams + indVal.numIndices] do
+      let arg := mkIdent (← mkFreshUserName `a)
+      indArgs := indArgs.push arg
+      let binder ← `(implicitBinderF| { $arg:ident })
+      binders := binders.push binder
+      if assumingParamIdxs.contains i then
+        let binder ← `(instBinderF| [ Inhabited $arg:ident ])
+        binders := binders.push binder
+    let type ← `(Inhabited (@$(mkIdent inductiveTypeName):ident $indArgs:ident*))
+    let mut ctorArgs := #[]
+    for i in [:ctorVal.numParams] do
+      ctorArgs := ctorArgs.push (← `(_))
+    for i in [:ctorVal.numFields] do
+      ctorArgs := ctorArgs.push (← ``(Inhabited.default))
+    let val ← `(⟨@$(mkIdent ctorName):ident $ctorArgs:ident*⟩)
+    `(instance $binders:explicitBinder* : $type := $val)
+
+  mkInstanceCmd? : TermElabM (Option Syntax) := do
+    let ctorVal ← getConstInfoCtor ctorName
+    forallTelescopeReducing ctorVal.type fun xs _ =>
+      addLocalInstancesForParams xs[:ctorVal.numParams] fun localInst2Index => do
+        let mut usedInstIdxs := {}
+        let mut ok := true
+        for i in [ctorVal.numParams:xs.size] do
+          let x := xs[i]
+          let instType ← mkAppM `Inhabited #[(← inferType x)]
+          trace[Elab.Deriving.inhabited] "checking {instType} for '{ctorName}'"
+          match (← trySynthInstance instType) with
+          | LOption.some e =>
+            usedInstIdxs := collectUsedLocalsInsts usedInstIdxs localInst2Index e
+          | _ =>
+            trace[Elab.Deriving.inhabited] "failed to generate instance using '{ctorName}' {if addHypotheses then "(assuming parameters are inhabited)" else ""} because of field with type{indentExpr (← inferType x)}"
+            ok := false
+            break
+        if !ok then
+          return none
+        else
+          trace[Elab.Deriving.inhabited] "inhabited instance using '{ctorName}' {if addHypotheses then "(assuming parameters are inhabited)" else ""} {usedInstIdxs.toList}"
+          let cmd ← mkInstanceCmdWith usedInstIdxs
+          trace[Elab.Deriving.inhabited] "\n{cmd}"
+          return some cmd
+
+private def mkInhabitedInstance (declName : Name) : CommandElabM Unit := do
+  let indVal ← getConstInfoInduct declName
+  let doIt (addHypotheses : Bool) : CommandElabM Bool := do
+    for ctorName in indVal.ctors do
+      if (← mkInhabitedInstanceUsing declName ctorName addHypotheses) then
+        return true
+    return false
+  unless (← doIt false <||> doIt true) do
+    throwError "failed to generate 'Inhabited' instance for '{declName}'"
+
+def mkInhabitedInstanceHandler (declNames : Array Name) : CommandElabM Bool := do
+  if (← declNames.allM isInductive) then
+    declNames.forM mkInhabitedInstance
+    return true
+  else
+    return false
+
+builtin_initialize
+  registerBuiltinDerivingHandler `Inhabited mkInhabitedInstanceHandler
+  registerTraceClass `Elab.Deriving.inhabited
+
+-/
+-- ^ END CUSTOM DERIVING
 
 /-
 macro_rules
