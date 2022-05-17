4bedd85970ad0c5ee51294d98a9efd7e162c1697
Andres Goens <andres.goens@ed.ac.uk>
2022-05-17 08:56:29 +0100

    read from command line (with a default)

--- Main.lean
+++ Main.lean
@@ -3,10 +3,13 @@ import Lean
 import PipelineDsl.Preprocess
 open Lean Pipeline
 
-def filename := "Examples/graph-prototype/operational-axiomatic/lsq-nosq/iter-1/load-controller.file"
--- def filename := "Examples/dummy-test.file"
+def default_filename := "Examples/graph-prototype/operational-axiomatic/lsq-nosq/iter-1/load-controller.file"
 
-def main : IO Unit := do
+def main (args : List String): IO Unit := do
+  let filename := match args.get? 0 with
+   | some fn => fn
+   | nothing => default_filename
+  println! s!"parsing {filename}"
   let lines <- IO.FS.lines filename
   let preprocessed := Preprocess lines
   let fileStr := preprocessed.foldl (λ s₁ s₂ => s₁ ++ "\n" ++ s₂) ""
@@ -18,4 +21,3 @@ def main : IO Unit := do
     | some msg => "syntax error:\n" ++ msg
     | none => s!"parse ok! round-trip: \n {parsed.2}"
   IO.println res_str
-  --IO.println s!"Read: {fileStr} \n Parsed: {parsed}"
