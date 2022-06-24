4f52d2d9812bdb67525fa3af6ae0dec41990b5f9
Andres Goens <andres.goens@ed.ac.uk>
2022-06-24 14:16:06 +0100

    commented/show fix on examples

--- Examples/tsfm-test.lean
+++ Examples/tsfm-test.lean
@@ -18,11 +18,24 @@ def ex0010 : Term := [dsl_term| hullo ]
 -- Why doesn't this accept matching on an object?
 open Pipeline.AST in
 def transform0001 ( input : Pipeline.AST) : Pipeline.Term :=
-match input with
-| Const => [dsl_term| "1" ]
+  match input with
+  | Const => [dsl_term| "1" ]
 
--- This doesn't work?
+#eval  transform0001 (AST.structure_descriptions [])
+
+-- The above doesn't really work. It's just taking in a name it doesn't know as a variable.
+-- Notice how the two get different highlighting (at least on my color scheme)
 open Pipeline.AST in
-def transform0002 ( input : Pipeline.AST) :=
+def transform0003 ( input : Pipeline.AST) : Pipeline.Term :=
+  match input with
+  | structure_descriptions [] => [dsl_term| "1" ]
+  | SomeRandomName => [dsl_term| "1" ]
+
+-- It doesn't work because Const is not (and was not) a constructor of AST.
+-- Remember that the AST type is just the root node.
+-- It works if you use the Const type instead of AST.
+open Pipeline.Const in
+def transform0002 ( input : Pipeline.Const) :=
 match input with
-| Pipeline.Const sth => sth 
+ | num_lit num => num + 1
+ | str_lit str => 0
