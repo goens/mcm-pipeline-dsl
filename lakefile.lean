16bf84b7fe718bf969f19a2cad95e6eaf9dcab02
An Qi Zhang <guy_asdf@hotmail.com>
2022-07-18 20:35:34 +0100

    Put in a commented out change to lakefile.lean if anyone needs to build on linux

--- lakefile.lean
+++ lakefile.lean
@@ -2,6 +2,13 @@ import Lake
 open Lake DSL
 
 package «pipeline-dsl» {
+
+  -- Uncomment the line about
+  -- supporting the interpreter if on Linux
+  -- See the following issue on Zulip
+  -- https://leanprover.zulipchat.com/#narrow/stream/270676-lean4/topic/include.20lean.2Eh/near/264879668
+  -- supportInterpreter := true
+
   -- add configuration options here
   dependencies := #[{
   name := `Graph
