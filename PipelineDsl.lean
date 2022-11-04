27b9c3f155225e3fc8b3de3a05a0ea956b5b6588
An Qi Zhang <guy_asdf@hotmail.com>
2022-11-04 21:06:33 +0000

    add the EmitMurphiNoROB to the PipelineDsl.lean file

--- PipelineDsl.lean
+++ PipelineDsl.lean
@@ -2,6 +2,7 @@ import PipelineDsl.AST
 import PipelineDsl.Parser
 import PipelineDsl.Preprocess
 import PipelineDsl.EmitMurphi
+import PipelineDsl.EmitMurphiNoROB
 import PipelineDsl.Translation
 import PipelineDsl.Transformation
 import PipelineDsl.LitmusTests
