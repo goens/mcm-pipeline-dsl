c6825767861d8f664f1f05525bb846a4de246f15
An Qi Zhang <guy_asdf@hotmail.com>
2023-02-27 20:17:47 +0000

    add load replay ideas

---
+++ PipelineDsl/CDFGLoadReplay.lean
@@ -0,0 +1,59 @@
+
+import PipelineDsl.AST
+import PipelineDsl.CDFG
+
+import PipelineDsl.DSLtoCDFG
+
+import PipelineDsl.CDFGAnalysis
+
+def CDFGLoadReplayTfsm (ctrlers : List controller_info)
+: Except String (List controller_info) := do
+  -- ** Get info about ctrlers for load replay
+  -- 1. Get the "Commit" Ctrler
+  -- 2. Search for where load API is called
+  -- 2.5 which ctrler awaits the load response
+  -- 3. Search for where the old load value is marked
+
+  -- ** Do the "start replay load"
+  -- A. If "Commit" and Load API are in the same ctrler then don't need to generate a msging line of code added.
+
+  -- If they are not in the same ctrler,
+
+  -- IF Load API is in a Queue:
+  -- IF the queue entries have a complete transition predicated by the Commit ctrler
+  -- place this await state before the state that the ctrler is in waiting for the commit ctrler...
+  -- add a msging line of code in the commit ctrler
+
+  -- If the queue entries do not have a complete transition predicated by the Commit ctrler
+  -- Throw an error, this is not a handled case
+
+  -- IF Load API is in a Ctrler:
+  -- Add an alternative await when case to the Ctrler to do the "replay load" actions
+
+  -- ** Awaiting the Memory Response
+  -- B.
+  -- if load API ctrler == await mem response ctrler,
+  --   add await mem response to this ctrler
+  --   Then see C. for adding comparison
+  -- if load API ctrler != await mem response ctrler,
+  --   add await mem response to the await mem response ctrler
+  --   NOTE: This assumes I can uniquely know which ctrler is awaiting this specific mem response, thru the seq_num, so ensure seq_nums are reset after complete...
+  --   But this should also go into a "replay state" so it knows to go to the replay states
+  --   Thus: add an await when in the ctrler that transitions to another state, to transition to a "replay" version that awaits the mem response
+  --   Then see C. for adding comparison
+
+  -- ** Comparison with Old Load Value
+  -- C.
+  --     if old load value is in the same ctrler as the load API,
+  --       add a comparison state or if expr here as well
+  --     if it's not in the same ctrler,
+  --       msg the old load value ctrler and add a comparison state there
+  --
+  --  If comparison is equal, signal to commit ctrler (if in a different ctrler than commit), if it's in the commit ctrler, just transition to next state (commit)
+  --  If comparison is not equal, signal to commit ctrler to squash.
+
+  -- ** Squash
+  -- D.
+  -- Msg Commit ctrler to squash if the comparison fails
+  
+  default
