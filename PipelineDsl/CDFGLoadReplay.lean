f3849d2f2024c4dcf8c5d8496a6564975bbd8b7b
An Qi Zhang <guy_asdf@hotmail.com>
2023-03-06 17:08:30 +0000

    start adding code to create the replay-issue & replay-await nodes

--- PipelineDsl/CDFGLoadReplay.lean
+++ PipelineDsl/CDFGLoadReplay.lean
@@ -6,18 +6,117 @@ import PipelineDsl.DSLtoCDFG
 
 import PipelineDsl.CDFGAnalysis
 
-def Ctrlers.replay_in_load_api_ctrler_state (ctrlers : Ctrlers) (graph : Graph) (load_api_state : CDFG.Node) : Except String Ctrlers := do
-  -- create a replay load state, simply sends the load request, then transitions to the next state
-  default
-
--- def CreateAwaitLoadResponseState (state_name : StateName) (transition_dest : StateName) (stmts : List Pipeline.Statement)
-
 -- I don't think we need multiple transitions (i.e. within the stmts, like in if-stmts or await-when stmts) just yet...
 structure SingleTransitionState where
 dest : StateName
 stmts : List Pipeline.Statement
 name : StateName
 deriving Inhabited, BEq
+
+-- The 4 nodes
+open CDFG in
+structure CommitIssueAwaitValueStmtNodes where
+commit_node : Node
+global_perform_load_node : Node
+global_complete_load_node : Node
+old_load_value_node : NodeLabelledStmt
+
+def replay_generated_prefix : String := "replay_generated_"
+
+open CDFG in
+def ReplayAwaitStateName (global_complete_load_node : Node) : StateName :=
+  let await_response_state_name := global_complete_load_node.current_state
+  let replay_await_state_name := replay_generated_prefix ++ await_response_state_name
+
+  replay_await_state_name
+
+def await_replay_start := "await_replay_start"
+
+def StartReplayAwaitMsgName (await_ctrler_name : CtrlerName) : String :=
+  await_replay_start ++ "_" ++ await_ctrler_name
+
+def replay_issue_load_to_mem := "replay_issue_load_to_mem"
+
+def CreateReplayIssueLoadState
+(is_issue_ctrler_and_await_response_ctrler_same : Bool)
+(is_issue_ctrler_pred_on_commit : Bool)
+-- (replay_await_state_name : StateName)
+(four_nodes : CommitIssueAwaitValueStmtNodes)
+(issue_ctrler_pred_on_commit? : Option CDFG.Node)
+(ctrlers : Ctrlers)
+: Except String (Pipeline.Description) := do
+  -- NOTE: Temporary way to get a "replay" load stmt
+  -- Proper way should read the load API usage, and copy any
+  -- dependent stmts
+  -- This would also then be a list of statments
+  let replay_load_stmt : Pipeline.Statement := CreateDSLMsgCall memory_interface load_perform []
+
+  let (transition_stmt, additional_stmts) : Pipeline.Statement × (List Pipeline.Statement) := ← 
+    if is_issue_ctrler_and_await_response_ctrler_same then
+      -- transition to the generated replay_await_response
+      let replay_await_state_name := ReplayAwaitStateName four_nodes.global_complete_load_node
+      let transition_to_replay_await : Pipeline.Statement := Pipeline.Statement.transition replay_await_state_name
+
+      pure (transition_to_replay_await, [])
+    else
+      -- Add msg stmt to the ctrler the await response is in
+      let await_ctrler_name := four_nodes.global_complete_load_node.ctrler_name
+      let start_replay_await_msg_name := StartReplayAwaitMsgName await_ctrler_name
+
+      let start_replay_await_msg := CreateDSLMsgCall await_ctrler_name start_replay_await_msg_name []
+
+      -- Generate the transition to the dest state
+      if is_issue_ctrler_pred_on_commit then -- && !is_issue_ctrler_and_await_response_ctrler_same
+        -- transition to existing await commit state in the issue load ctrler
+        if let some issue_ctrler_pred_on_commit := issue_ctrler_pred_on_commit? then
+          let issue_ctrler_state_that's_pred_on_commit := issue_ctrler_pred_on_commit.current_state
+          let transition_to_issue_ctrler_state := Pipeline.Statement.transition issue_ctrler_state_that's_pred_on_commit
+
+          pure (transition_to_issue_ctrler_state, [start_replay_await_msg])
+        else
+          throw "While CreateReplayIssueLoadState, couldn't find the state issue is predicated by commit on"
+      else do -- !is_issue_ctrler_pred_on_commit && !is_issue_ctrler_and_await_response_ctrler_same
+        -- transition to the first state in the issue ctrler
+        let issue_ctrler : Ctrler ← ctrlers.ctrler_from_name four_nodes.global_perform_load_node.ctrler_name
+        let issue_ctrler_first_state : StateName ← issue_ctrler.init_trans_dest
+        let transition_to_first_state := Pipeline.Statement.transition issue_ctrler_first_state
+
+        pure (transition_to_first_state, [start_replay_await_msg])
+  
+  let replay_issue_stmts_blk := Pipeline.Statement.block ([replay_load_stmt] ++ additional_stmts ++ [transition_stmt])
+  let replay_issue_name := replay_issue_load_to_mem
+  let replay_state := Pipeline.Description.state replay_issue_name replay_issue_stmts_blk
+
+  pure replay_state
+
+def CreateReplayAwaitLoadState
+(is_issue_ctrler_and_await_response_ctrler_same : Bool)
+(is_issue_ctrler_pred_on_commit : Bool)
+(four_nodes : CommitIssueAwaitValueStmtNodes)
+: Except String Pipeline.Description := do
+  -- ===== required stmts: =====
+  -- 1. await the load response
+  -- Just simply use an await stmt for now
+  -- Should try to copy the original await stmt and handling code
+  let when_load_response_stmt := Pipeline.Statement.when [memory_interface, load_completed].to_qual_name []
+  let await_load_response := Pipeline.Statement.await none []
+
+  -- 2. read the old value that was marked
+  -- look at the old value stmt, if it's from the reg file, then read from the reg file
+  -- if it's from a state var, then this is hard to say... maybe we read from the start var
+  let old_load_value_stmt := four_nodes.old_load_value_node.labelled_stmt
+
+  -- 3. compare, handle 2 cases
+  --   a. old val == new val: no extra stmts
+  --   b. old val != new val: write to where the value is stored
+  --   NOTE: This may be more complicated, if the user marks a state var assignment, but
+  --   the real place to write to is the register file...
+
+  -- 4. signal to ROB that replay is complete
+
+  -- ===== Then handle the transition at the end =====
+
+  default
 
 def CDFG.Graph.AddLoadReplayToCtrlers (graph : Graph) (ctrlers : Ctrlers) : Except String (Ctrlers) := do
   -- Get the relevant 4 states & ctrlers
@@ -90,11 +189,12 @@ def CDFG.Graph.AddLoadReplayToCtrlers (graph : Graph) (ctrlers : Ctrlers) : Exce
   --   Murphi NOTE: This assumes I can uniquely know which ctrler is awaiting this specific mem response, thru the seq_num, so ensure seq_nums are reset after complete...
   --   Need 2 states: 1. for moving to a state that specifically awaits for load-replay's response, 2. for receiving the response.
   --   Then see C. for adding comparison
-  match is_load_api_and_await_load_in_same_ctrler with
-  | true => -- create a state to await the mem response
-    let when_stmt := Pipeline.Statement.when [memory_interface, load_completed].to_qual_name [load_value] compare_and_transition_stmt_blk
-    let await_stmt := Pipeline.Statement.await none when_stmt
-  | false =>
+
+  -- match is_load_api_and_await_load_in_same_ctrler with
+  -- | true => -- create a state to await the mem response
+  --   let when_stmt := Pipeline.Statement.when [memory_interface, load_completed].to_qual_name [load_value] compare_and_transition_stmt_blk
+  --   let await_stmt := Pipeline.Statement.await none when_stmt
+  -- | false =>
 
   -- let await_replay_mem_states : List Pipeline.Description :=
   --   if is_load_api_and_await_load_in_same_ctrler then
