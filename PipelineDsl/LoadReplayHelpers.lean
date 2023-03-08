39717bf4aa9cbaed431adfc7555affd2697c8a6b
An Qi Zhang <guy_asdf@hotmail.com>
2023-03-08 20:40:26 +0000

    add original_commit_code_prefix to mark the original commit code when an inst is ready to commit

--- PipelineDsl/LoadReplayHelpers.lean
+++ PipelineDsl/LoadReplayHelpers.lean
@@ -3,3 +3,5 @@
 def old_load_value := "old_load_value"
 def replay_complete_msg_to_commit := "replay_complete"
 def replay_value := "replay_value"
+
+def original_commit_code_prefix := "original_commit"
