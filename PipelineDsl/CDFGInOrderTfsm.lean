e6888a38914a1da884d8cb5222f7839dc9a82bec
An Qi Zhang <guy_asdf@hotmail.com>
2023-03-31 23:13:02 +0100

    try to use part of the new in order transformation code (work in progress)

--- PipelineDsl/CDFGInOrderTfsm.lean
+++ PipelineDsl/CDFGInOrderTfsm.lean
@@ -6,7 +6,7 @@ import PipelineDsl.DSLtoCDFG
 open CDFG
 
 -- TODO: Check for other ctrlers the inst to stall on can be in
-def CDFGInOrderTfsm (ctrlers : List controller_info) (inst_to_stall_on_type : InstType) (inst_to_stall_type : InstType)
+def CDFG.InOrderTfsm (ctrlers : List controller_info) (inst_to_stall_on_type : InstType) (inst_to_stall_type : InstType)
 : Except String (List controller_info) := do
   dbg_trace "<< Starting CDFGInOrderTfsm"
   let graph_nodes ← DSLtoCDFG ctrlers
@@ -14,21 +14,29 @@ def CDFGInOrderTfsm (ctrlers : List controller_info) (inst_to_stall_on_type : In
   let graph := {nodes := graph_nodes}
   let stall_point ← find_stall_point_heuristic graph inst_to_stall_type ctrlers
   dbg_trace s!"<< Found stall point from heuristic: ({stall_point})"
-  let ctrler_state_to_stall_on : StateOrConstraintToStallOn ← find_ctrler_or_state_to_query_for_stall graph inst_to_stall_on_type ctrlers
-  dbg_trace "<< Found ctrler/state to stall at"
 
-  let stall_node ← CreateStallNode stall_point ctrler_state_to_stall_on ctrlers inst_to_stall_on_type inst_to_stall_type
+  -- === Old code to find stall state ===
+  -- let ctrler_state_to_stall_on : StateOrConstraintToStallOn ← find_ctrler_or_state_to_query_for_stall graph inst_to_stall_on_type ctrlers
+  -- dbg_trace "<< Found ctrler/state to stall at"
 
-  let graph_constrained_by_second_inst := ← graph.states_the_'to_stall_on'_node_can_be_in inst_to_stall_on_type inst_to_stall_type stall_point ctrlers
-  dbg_trace s!"======= BEGIN graph constrained by second inst ========"
-  dbg_trace s!"BEGIN graph_constrained_by_second_inst: ({graph_constrained_by_second_inst.nodes.map (·.current_state)})\nEND graph_constrained_by_second_inst"
-  dbg_trace s!"======= END graph constrained by second inst ========"
+  -- let stall_node ← CreateStallNode stall_point ctrler_state_to_stall_on ctrlers inst_to_stall_on_type inst_to_stall_type
+  -- === Old code to find stall state ===
 
-  let graph_pruned_of_live_subsets := ← graph.prune_ctrler_nodes_that_are_live_for_a_subset_of_another_ctrler inst_to_stall_on_type
-    |>.throw_exception_nesting_msg s!"Error pruning graph of live subsets. Graph: ({graph_constrained_by_second_inst.nodes.map (·.current_state)})"
-  dbg_trace s!"======= BEGIN graph_pruned_of_live_subsets ========"
-  dbg_trace s!"BEGIN graph_pruned_of_live_subsets: ({graph_pruned_of_live_subsets.nodes.map (·.current_state)})\nEND graph_pruned_of_live_subsets"
-  dbg_trace s!"======= END graph_pruned_of_live_subsets ========"
+  -- === New code to find stall state ===
+  let nodes_to_query ← graph.find_pre_receive_stall_states inst_to_stall_on_type
+  
+  -- === New code to find stall state ===
+
+  -- let graph_constrained_by_second_inst := ← graph.states_the_'to_stall_on'_node_can_be_in inst_to_stall_on_type inst_to_stall_type stall_point ctrlers
+  -- dbg_trace s!"======= BEGIN graph constrained by second inst ========"
+  -- dbg_trace s!"BEGIN graph_constrained_by_second_inst: ({graph_constrained_by_second_inst.nodes.map (·.current_state)})\nEND graph_constrained_by_second_inst"
+  -- dbg_trace s!"======= END graph constrained by second inst ========"
+
+  -- let graph_pruned_of_live_subsets := ← graph.prune_ctrler_nodes_that_are_live_for_a_subset_of_another_ctrler inst_to_stall_on_type
+  --   |>.throw_exception_nesting_msg s!"Error pruning graph of live subsets. Graph: ({graph_constrained_by_second_inst.nodes.map (·.current_state)})"
+  -- dbg_trace s!"======= BEGIN graph_pruned_of_live_subsets ========"
+  -- dbg_trace s!"BEGIN graph_pruned_of_live_subsets: ({graph_pruned_of_live_subsets.nodes.map (·.current_state)})\nEND graph_pruned_of_live_subsets"
+  -- dbg_trace s!"======= END graph_pruned_of_live_subsets ========"
 
   let new_state_name := stall_point.ctrler ++ "_stall_" ++ stall_point.state
   let updated_ctrlers ← UpdateCtrlerWithNode ctrlers stall_point.ctrler new_state_name stall_node stall_point.state
