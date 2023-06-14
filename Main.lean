7ac6c4baf72c203e9c9a914aff8778d42ddf2759
An Qi Zhang <guy_asdf@hotmail.com>
2023-06-14 17:40:02 +0100

    use transforms for TSO on LQ SQ SB LSQ

--- Main.lean
+++ Main.lean
@@ -130,12 +130,27 @@ def transformTesting : AST → Array Nat → IO Unit
       --     dbg_trace s!"Error applying st->ld in CDFG InOrderTfsm: ({msg})"
       --     []
 
-      -- let st_st := ( MCMOrdering.binary_ordering (BinaryOrdering.mk [ store' ] [ store' ] Addresses.any) )
-      -- let ctrlers := match CDFG.InOrderTransform ctrlers st_st none with
-      --   | .ok ctrler_list => ctrler_list
-      --   | .error msg => 
-      --     dbg_trace s!"Error applying st->st in CDFG InOrderTfsm: ({msg})"
-      --     []
+      let ld_ld := ( MCMOrdering.binary_ordering (BinaryOrdering.mk [ load' ] [ load' ] Addresses.any) )
+      let ctrlers := match CDFG.InOrderTransform ctrlers ld_ld none with
+      | .ok ctrler_list => ctrler_list
+      | .error msg => 
+      dbg_trace s!"Error applying ld->ld in CDFG InOrderTfsm: ({msg})"
+      []
+
+      let st_st := ( MCMOrdering.binary_ordering (BinaryOrdering.mk [ store' ] [ store' ] Addresses.any) )
+      let ctrlers := match CDFG.InOrderTransform ctrlers st_st none with
+        | .ok ctrler_list => ctrler_list
+        | .error msg => 
+          dbg_trace s!"Error applying st->st in CDFG InOrderTfsm: ({msg})"
+          []
+
+      let ld_st_fence_ld_st := ( MCMOrdering.ternary_ordering
+        (TernaryOrdering.mk [ load', store' ] mfence' [ load', store' ] Addresses.any) )
+      let ctrlers := match CDFG.InOrderTransform ctrlers ld_st_fence_ld_st none with
+        | .ok ctrler_list => ctrler_list
+        | .error msg => 
+          dbg_trace s!"Error applying st->mfence in CDFG InOrderTfsm: ({msg})"
+          []
 
       -- let st_st := ( MCMOrdering.binary_ordering (BinaryOrdering.mk [ store' ] [ store' ] Addresses.any) )
       -- let ctrlers := match CDFG.InOrderTransform ctrlers st_st none with
@@ -150,12 +165,12 @@ def transformTesting : AST → Array Nat → IO Unit
       --     dbg_trace s!"Error adding invalidation listener: ({msg})"
       --     []
           
-      let st_ld'_fence_ld := ( MCMOrdering.ternary_ordering (TernaryOrdering.mk [ store', load' ] mfence' [ load' ] Addresses.any) )
-      let ctrlers := match Ctrlers.CDFGLoadReplayTfsm ctrlers st_ld'_fence_ld with
-        | .ok ctrler_list => ctrler_list
-        | .error msg => 
-          dbg_trace s!"Error applying Load-Replay ld->ld in CDFG LoadReplayTfsm: ({msg})"
-          []
+      -- let st_ld'_fence_ld := ( MCMOrdering.ternary_ordering (TernaryOrdering.mk [ store', load' ] mfence' [ load' ] Addresses.any) )
+      -- let ctrlers := match Ctrlers.CDFGLoadReplayTfsm ctrlers st_ld'_fence_ld with
+      --   | .ok ctrler_list => ctrler_list
+      --   | .error msg => 
+      --     dbg_trace s!"Error applying Load-Replay ld->ld in CDFG LoadReplayTfsm: ({msg})"
+      --     []
 
       -- let load_replay_ld_ld := ( MCMOrdering.ternary_ordering (TernaryOrdering.mk [ load' ] mfence' [ load' ] Addresses.any) )
       -- let ctrlers := match Ctrlers.CDFGLoadReplayTfsm ctrlers none with
