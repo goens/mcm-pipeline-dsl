fb5028750b0e9fdba4dfe44a367ee5efd769b206
An Qi Zhang <aq2zhang@uwaterloo.ca>
2024-06-30 14:45:15 +0100

    add tso file for io transforms for LB and Unified LSQs, and arm file for LB and Unified with LR transforms

---
+++ artifact-arm-lsq-tfsm-configs/LR/TransformsToApply_LB_and_Unified_ARM_IO_and_LR.lean
@@ -0,0 +1,32 @@
+
+import PipelineDsl.ApplyTransformations
+
+def transforms := [
+  --(Transformation.IO,
+  -- MCMOrdering.binary_ordering
+  --   (BinaryOrdering.mk [ ldar ] [ load ] Addresses.any) ),
+  --(Transformation.IO,
+  -- MCMOrdering.binary_ordering
+  --   (BinaryOrdering.mk [ ldar ] [ ldar ] Addresses.any) ),
+  --  (Transformation.IO,
+  --   MCMOrdering.binary_ordering
+  --     (BinaryOrdering.mk [ store, stlr ] [ stlr ] Addresses.any) ),
+  --  (Transformation.IO,
+  --   MCMOrdering.binary_ordering
+  --     (BinaryOrdering.mk [ store, stlr ] [ dmb_st ] Addresses.any) ),
+  --  (Transformation.IO,
+  --   MCMOrdering.binary_ordering
+  --     (BinaryOrdering.mk [ store, stlr ] [ dmb_sy ] Addresses.any) ),
+  --  (Transformation.IO,
+  --   MCMOrdering.binary_ordering
+  --     (BinaryOrdering.mk [ store, stlr ] [ dmb_sy ] Addresses.any) ),
+  --(Transformation.IO,
+  -- MCMOrdering.binary_ordering
+  --   (BinaryOrdering.mk [ dmb_ld ] [ ldar ] Addresses.any) ),
+  --(Transformation.IO,
+  -- MCMOrdering.binary_ordering
+  --   (BinaryOrdering.mk [ dmb_ld ] [ load ] Addresses.any) )
+  (Transformation.LR,
+   MCMOrdering.ternary_ordering
+     (TernaryOrdering.mk [ load' ] dmb_sy' [ load' ] Addresses.any) )
+]
