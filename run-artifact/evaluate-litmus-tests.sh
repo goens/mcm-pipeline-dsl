42ad64217b9adcf55a076c537592d0ea0b9f5250
An Qi Zhang <aq2zhang@uwaterloo.ca>
2024-07-01 22:22:52 +0100

    add comments for what the artifact script should do

---
+++ run-artifact/evaluate-litmus-tests.sh
@@ -0,0 +1,5 @@
+#!/bin/zsh
+
+# (1) Copy in the LSQ we want to test transformations with
+# (2) copy in the transformations to apply
+# (3) Run each litmus test, and collect it's result (allowed or disallowed)
