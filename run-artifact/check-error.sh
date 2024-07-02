0d49cd9ebf6c4678cb4f744780fdcdaf7e1916ee
An Qi Zhang <aq2zhang@uwaterloo.ca>
2024-07-02 15:49:26 +0100

    add files to test checking the output of grep (return code 0 or 1), doesn't seem to be working?

--- run-artifact/check-error.sh
+++ run-artifact/check-error.sh
@@ -1,8 +1,8 @@
 #!/bin/zsh
 
-if grep -q $'Status:\n\n\tNo error found.' test-HP-arm-io-it/armv8-HP-IT-amd1-dmb-sy.out.run; then
+if grep -q $'Status:\n\n\tNo error found.' armv8-HP-IT-amd1.out.run; then
   echo "Unobserved Ordering"
-elif grep -q $'Result:\n\n\tInvariant ".*" failed.' test-HP-arm-io-it/armv8-HP-IT-amd1-dmb-sy.out.run; then
+elif grep -q $'Result:\n\n\tInvariant ".*" failed.' armv8-HP-IT-amd1.out.run; then
   echo "Observed Ordering"
 else
   echo "Unexpected error occured."
