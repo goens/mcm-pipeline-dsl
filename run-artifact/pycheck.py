8a88076a307ffe3b2100330d4005fea8f0a4094a
An Qi Zhang <aq2zhang@uwaterloo.ca>
2024-07-02 19:47:48 +0100

    update python file that uses a shell command/script to check if a file contains specific string

--- run-artifact/pycheck.py
+++ run-artifact/pycheck.py
@@ -3,7 +3,7 @@
 import subprocess
 
 # result = subprocess.run("grep -q \"No error found.\" armv8-HP-IT-amd1-dmb-sy.out.run", shell=True)
-result = subprocess.run("if grep -q \"No error found.\" armv8-HP-IT-amd1.out.run; then exit 0; else exit 1; fi", shell=True)
+result = subprocess.run("if grep -q \"\tNo error found.\" armv8-HP-IT-amd1.out.run; then exit 0; else exit 1; fi", shell=True)
 # result = subprocess.run("if grep -q $'Status:\n\n\tNo error found.' armv8-HP-IT-amd1-dmb-sy.out.run; then exit 0; else exit 1; fi", shell=True)
 
 print(result.returncode)
