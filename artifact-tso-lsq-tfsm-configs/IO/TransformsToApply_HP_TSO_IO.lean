04e8baa630a2d34d964a4f452aac2c1ac260c1b1
An Qi Zhang <aq2zhang@uwaterloo.ca>
2024-07-04 15:41:10 +0100

    rename and add IO TSO transform files

---
+++ artifact-tso-lsq-tfsm-configs/IO/TransformsToApply_HP_TSO_IO.lean
@@ -0,0 +1,48 @@
+
+import PipelineDsl.ApplyTransformations
+
+def transforms := [
+  (Transformation.IO,
+   MCMOrdering.binary_ordering
+    (BinaryOrdering.mk [ load ] [ load ] Addresses.any) ),
+  (Transformation.IO,
+   MCMOrdering.ternary_ordering
+    (TernaryOrdering.mk [ load' ] mfence' [ load' ] Addresses.any) ),
+  (Transformation.IO,
+   MCMOrdering.binary_ordering
+    (BinaryOrdering.mk [ store ] [ store ] Addresses.any) )
+
+  --(Transformation.IO,
+  -- MCMOrdering.binary_ordering
+  --   (BinaryOrdering.mk [ ldar ] [ load ] Addresses.any) ),
+  --(Transformation.IO,
+  -- MCMOrdering.binary_ordering
+  --   (BinaryOrdering.mk [ ldar ] [ ldar ] Addresses.any) ),
+
+  --  (Transformation.IO,
+  --   MCMOrdering.binary_ordering
+  --     (BinaryOrdering.mk [ store, stlr ] [ stlr ] Addresses.any) ),
+  --  (Transformation.IO,
+  --   MCMOrdering.binary_ordering
+  --     (BinaryOrdering.mk [ store, stlr ] [ dmb_st ] Addresses.any) ),
+  --  (Transformation.IO,
+  --   MCMOrdering.binary_ordering
+  --     (BinaryOrdering.mk [ store, stlr ] [ dmb_sy ] Addresses.any) ),
+  --  (Transformation.IO,
+  --   MCMOrdering.binary_ordering
+  --     (BinaryOrdering.mk [ store, stlr ] [ dmb_sy ] Addresses.any) ),
+
+  --(Transformation.IO,
+  -- MCMOrdering.binary_ordering
+  --   (BinaryOrdering.mk [ dmb_ld ] [ ldar ] Addresses.any) ),
+  --(Transformation.IO,
+  -- MCMOrdering.binary_ordering
+  --   (BinaryOrdering.mk [ dmb_ld ] [ load ] Addresses.any) )
+  -- (Transformation.LR,
+  --  MCMOrdering.ternary_ordering
+  --    (TernaryOrdering.mk [ load' ] dmb_sy' [ load' ] Addresses.any) )
+
+  -- (Transformation.IT, -- ordering below not used.
+  --  MCMOrdering.ternary_ordering
+  --    (TernaryOrdering.mk [ load' ] mfence' [ load' ] Addresses.any) )
+]
